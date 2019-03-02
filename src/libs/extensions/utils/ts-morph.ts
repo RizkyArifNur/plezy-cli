b7e45cedac7be8c23b8680f98bc826cafdfb953e
Rizky Arif <rizkyarifnur@gmail.com>
2019-03-03 06:39:41 +0700

    adding @types/ramda and add function addNamedImports

--- src/libs/extensions/utils/ts-morph.ts
+++ src/libs/extensions/utils/ts-morph.ts
@@ -1,5 +1,6 @@
 import { GluegunToolbox } from 'gluegun'
 import Project, { SourceFile } from 'ts-morph'
+import * as R from 'ramda'
 
 export class TsMorph {
   context: GluegunToolbox
@@ -13,5 +14,24 @@ export class TsMorph {
     this.context = context
     this.project = project
     this.sourceFile = project.getSourceFile(cwd(filePath).cwd())
+  }
+
+  addNamedImports(fromModule: string, namedImports: string[]) {
+    const { sourceFile } = this
+
+    let existingNamedImports = []
+    const existings = sourceFile.getImportDeclaration(fromModule)
+
+    if (existings) {
+      existingNamedImports = existings
+        .getNamedImports()
+        .map(nameImport => nameImport.getText)
+      existings.remove()
+    }
+
+    sourceFile.addImportDeclaration({
+      moduleSpecifier: fromModule,
+      namedImports: R.uniq([...namedImports, ...existingNamedImports]).sort()
+    })
   }
 }
