a58bed54d0bf854dbedc88e68f87b3f7a6ea0359
Rizky Arif <rizkyarifnur@gmail.com>
2019-03-03 06:33:29 +0700

    adding ramda

--- src/libs/extensions/utils/ts-morph.ts
+++ src/libs/extensions/utils/ts-morph.ts
@@ -1,9 +1,17 @@
 import { GluegunToolbox } from 'gluegun'
-import Project from 'ts-morph'
+import Project, { SourceFile } from 'ts-morph'
 
 export class TsMorph {
   context: GluegunToolbox
+  project: Project
+  sourceFile: SourceFile
+  filePath: string
   constructor(context: GluegunToolbox, project: Project, filePath: string) {
+    const {
+      filesystem: { cwd }
+    } = context
     this.context = context
+    this.project = project
+    this.sourceFile = project.getSourceFile(cwd(filePath).cwd())
   }
 }
