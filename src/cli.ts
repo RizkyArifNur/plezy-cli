d4f19c850c9bd57550817e9e2951ffa922810c9f
Rizky Arif <rizkyarifnur@gmail.com>
2019-03-02 17:33:10 +0700

    initial commit

---
+++ src/cli.ts
@@ -0,0 +1,24 @@
+import { build } from 'gluegun'
+
+/**
+ * Create the cli and kick it off
+ */
+async function run(argv) {
+  // create a CLI runtime
+  const cli = build()
+    .brand('plezy')
+    .src(__dirname)
+    .plugins('./node_modules', { matching: 'plezy-*', hidden: true })
+    .help() // provides default for help, h, --help, -h
+    .version() // provides default for version, v, --version, -v
+
+    .create()
+
+  // and run it
+  const toolbox = await cli.run(argv)
+
+  // send it back (for testing, mostly)
+  return toolbox
+}
+
+module.exports = { run }
